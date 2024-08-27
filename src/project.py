ca613c70b405c63b76a68806ef5a9a35f44e2c71
gv <afairless@gmail.com>
2024-08-26 21:55:21 -0400

    set up time series trend cut points

--- src/project.py
+++ src/project.py
@@ -3,9 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima_process import ArmaProcess
-from statsmodels.tsa.deterministic import DeterministicProcess
-
-from pandas import date_range
+# from statsmodels.tsa.deterministic import DeterministicProcess
 
 
 def dummy_function01() -> int:
@@ -26,6 +24,28 @@ def dummy_function02(input: int) -> int:
     return input + 1
 
 
+def randomize_segment_lengths(
+    total_n: int, segment_n: int, seed=282840) -> np.ndarray:
+    """
+    Divide 'total_n' into 'segment_n' segments and randomize the lengths of the
+        segments
+    """
+
+    segment_even_len = total_n // segment_n
+    segment_len_vary = int(np.floor(segment_even_len * 0.9))
+
+    np.random.seed(seed)
+    segment_lens = np.random.uniform(
+        segment_even_len - segment_len_vary, 
+        segment_even_len + segment_len_vary, 
+        segment_n)
+    np.append(segment_lens, total_n - np.sum(segment_lens))
+
+    assert (segment_lens > 0).all()
+
+    return segment_lens
+
+
 def main():
 
     # index = date_range('2000-1-1', freq='M', periods=240)
@@ -33,18 +53,28 @@ def main():
     #     index=index, constant=True, period=3, order=2, seasonal=True)
     # dtrm_pd = dtrm_process.in_sample()
 
+    # alterations in series parameters:
+    #   use trig function for trend to change slope
+    #   arbitrarily flatten trend
+    #   shift constant
+
+
     n_samples = 1000
+    constant = np.zeros(n_samples)
+
+    trend_n = 5
+    trend_lens = randomize_segment_lengths(n_samples, trend_n)
+
     trend_slope = 0.05
-    seasonality_period = 500
+
+    season_period = 500
     sin_amplitude = 10
     cos_amplitude = -10
 
     time_idx = np.arange(n_samples)
     trend = trend_slope * time_idx
-    sin_seasonality = (
-        sin_amplitude * np.sin(2 * np.pi * time_idx / seasonality_period))
-    cos_seasonality = (
-        cos_amplitude * np.cos(2 * np.pi * time_idx / seasonality_period))
+    season_sin = sin_amplitude * np.sin(2 * np.pi * time_idx / season_period)
+    season_cos = cos_amplitude * np.cos(2 * np.pi * time_idx / season_period)
 
     ar = np.array([1, 0.9])
     ma = np.array([1, 1.9])
@@ -53,7 +83,7 @@ def main():
     np.random.seed(374352)
     arma_noise = arma_process.generate_sample(nsample=n_samples)
 
-    time_series = trend + sin_seasonality + cos_seasonality + arma_noise
+    time_series = constant + trend + season_sin + season_cos + arma_noise
 
     plt.clf()
     plt.close()
