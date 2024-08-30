7d64f371909abccf3f0f788442014a86c6673ea7
gv <afairless@gmail.com>
2024-08-30 10:26:47 -0400

    remove extraneous code

--- src/s01_generate_data.py
+++ src/s01_generate_data.py
@@ -1,13 +1,11 @@
 #! /usr/bin/env python3
 
-import math
 import polars as pl
 from dataclasses import dataclass, fields
 from typing import Any
 
 import numpy as np
 from scipy.stats import dirichlet
-import statsmodels.api as sm
 from statsmodels.tsa.arima_process import ArmaProcess
 # from statsmodels.tsa.deterministic import DeterministicProcess
 
@@ -64,54 +62,6 @@ class TimeSeriesParameters:
         assert self.autogressive_lag_polynomial_coefficients[0] == 1
         assert self.moving_average_lag_polynomial_coefficients[0] == 1
         assert self.arma_scale > 0
-
-
-def create_time_series_01(
-    n: int, n_trends: int, 
-    autogressive_order: int=3, moving_average_order: int=2, 
-    arma_std_factor: float=4, season_std_factor: float=9, 
-    seed: int=231562) -> np.ndarray:
-    """
-    Create time series data with multiple trends and seasonality added to output
-        from ARMA model
-    """
-
-    np.random.seed(seed)
-    arma = sm.tsa.arma_generate_sample(
-        autogressive_order, moving_average_order, n, burnin=max(20, n//10))
-
-    # add multiple trends across time series
-    trend_len_mean = n / n_trends
-    trend_len_min = math.floor(trend_len_mean / 2)
-    trend_len_max = math.ceil(trend_len_mean * 2)
-    trend_lens = np.random.randint(trend_len_min, trend_len_max, n_trends)
-    trend_lens = np.round((trend_lens / trend_lens.sum()) * n).astype(int)
-    trend_lens[-1] += n - trend_lens.sum()
-    assert trend_lens.sum() == n
-
-    trend_slopes = np.random.uniform(low=-3, high=3, size=n_trends)
-
-    assert len(trend_lens) == len(trend_slopes)
-    trends_by_n = [
-        range(1, trend_lens[i]+1) * trend_slopes[i] 
-        for i in range(n_trends)]
-    trend_ends = [e[-1] for e in trends_by_n]
-    trend_continues = [0] + trend_ends[:-1]
-    trend_continues = np.array(trend_continues).cumsum()
-    trends = np.concatenate([
-        e + trend_continues[i] 
-        for i, e in enumerate(trends_by_n)])
-
-    season = np.sin(np.pi * np.arange(n) / 6)
-
-    # re-scale ARMA output and seasonality
-    arma_factor = np.std(trends) / arma_std_factor
-    season_factor = np.std(trends) / season_std_factor 
-
-    # assemble all components of time series
-    time_series = arma_factor * arma + season_factor * season + trends
-
-    return time_series
 
 
 def randomize_segment_lengths(
@@ -175,7 +125,7 @@ def generate_and_combine_trends(
     return trend_segments 
 
 
-def create_time_series_02(
+def create_time_series(
     time_n: int=100, series_n: int=1, constant: np.ndarray=np.zeros(100),
     trend_n: int=1, trend_slope_min: float=-1, trend_slope_max: float=1,
     season_period: int=10, sin_amplitude: float=1, cos_amplitude: float=1, 
@@ -183,8 +133,8 @@ def create_time_series_02(
     moving_average_lag_polynomial_coefficients: np.ndarray=np.array([1, 1]), 
     arma_scale: float=1, seed: int=231562) -> TimeSeriesTrendSegments:
     """
-    Create time series data with multiple trends and seasonality added to output
-        from ARMA model
+    Generate time series data with multiple trends and seasonality added to 
+        output from ARMA model
     """
 
     assert len(constant) == time_n
@@ -242,7 +192,7 @@ def create_time_series_02(
     return time_series_with_trends 
 
 
-def generate_time_series_with_params() -> TimeSeriesParameters:
+def create_time_series_with_params() -> TimeSeriesParameters:
     """
     Generate time series data with specified parameters for trends, seasonality, 
         ARMA error, etc. and return the parameters and series packaged together
@@ -253,10 +203,6 @@ def generate_time_series_with_params() -> TimeSeriesParameters:
     # dtrm_process = DeterministicProcess(
     #     index=index, constant=True, period=3, order=2, seasonal=True)
     # dtrm_pd = dtrm_process.in_sample()
-
-    # n = 400
-    # n_trends = 7
-    # srs = create_time_series_01(n, n_trends, 3, 2, 4, 9, 84558)
 
     time_n = 100
     series_n = 2
@@ -278,7 +224,7 @@ def generate_time_series_with_params() -> TimeSeriesParameters:
     arma_scale = 20
 
     seed = 761824
-    time_series = create_time_series_02(
+    time_series = create_time_series(
         time_n, series_n, constant, 
         trend_n, trend_slope_min, trend_slope_max, 
         season_period, sin_amplitude, cos_amplitude, 
@@ -352,7 +298,7 @@ def convert_time_series_parameters_to_dataframe(
 
 def main():
 
-    ts_params = generate_time_series_with_params()
+    ts_params = create_time_series_with_params()
     ts_params_df = convert_time_series_parameters_to_dataframe(ts_params)
 
     ts = ts_params.time_series
