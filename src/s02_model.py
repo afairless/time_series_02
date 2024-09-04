be1f107561919162a014986c5615f656fb6ac11a
gv <afairless@gmail.com>
2024-09-04 13:46:29 -0400

    add KPSS test

--- src/s02_model.py
+++ src/s02_model.py
@@ -7,10 +7,17 @@ import matplotlib.pyplot as plt
 
 import statsmodels.tsa.stattools as tsa_tools
 
-from src.s01_generate_data import (
-    expand_values_by_lengths_into_vector,
-    plot_time_series,
-    )
+if __name__ == '__main__':
+    from s01_generate_data import (
+        expand_values_by_lengths_into_vector,
+        plot_time_series,
+        )
+
+else:
+    from src.s01_generate_data import (
+        expand_values_by_lengths_into_vector,
+        plot_time_series,
+        )
 
 
 def write_list_to_text_file(
@@ -106,10 +113,18 @@ def main():
 
     ts_colnames = [e for e in df.columns if e[:3] == 'ts_']
 
+
+    # plot full time series
+    ################################################## 
+
+    md.append('## Plot full time series')
+    md.append('\n')
+
     ts = df[row_idx, ts_colnames].to_numpy().reshape(-1)
     plot_filepath = output_path / 'time_series.png'
-    plot_time_series(ts.reshape(1, -1), 1, plot_filepath)
-    md.append('Full time series')
+    ts_and_trend = np.vstack([ts, trend])
+    plot_time_series(ts_and_trend, 2, plot_filepath)
+    md.append('Full time series with true trend')
     md.append('\n')
     md.append(f'![Image]({plot_filepath.name})')
     md.append('\n')
@@ -122,17 +137,133 @@ def main():
     md.append(f'![Image]({plot_filepath.name})')
     md.append('\n')
 
-    ts_train = ts[:test_start_idx]
-    detrend_ts_train = detrend_ts[:test_start_idx]
 
-    
+    # plot time series training segment
+    ################################################## 
+
+    md.append('## Plot time series training segment')
+    md.append('\n')
+
+    ts_train = ts[:test_start_idx]
+    plot_filepath = output_path / 'time_series_train.png'
+    plot_time_series(ts_train.reshape(1, -1), 1, plot_filepath)
+    md.append('Time series training segment')
+    md.append('\n')
+    md.append(f'![Image]({plot_filepath.name})')
+    md.append('\n')
+
+    detrend_ts_train = detrend_ts[:test_start_idx]
+    plot_filepath = output_path / 'time_series_train_detrend.png'
+    plot_time_series(detrend_ts_train.reshape(1, -1), 1, plot_filepath)
+    md.append('Detrended time series training segment')
+    md.append('\n')
+    md.append(f'![Image]({plot_filepath.name})')
+    md.append('\n')
+
+    md.append(f'Length of time series training segment:  {len(ts_train)}')
+    md.append('\n')
 
     md.append(
+        'https://www.statsmodels.org/dev/examples/notebooks/generated/stationarity_detrending_adf_kpss.html')
+    md.append('\n')
+
+    # 
+    ################################################## 
+    # https://otexts.com/fpp3/stationarity.html
+    # https://www.statsmodels.org/dev/examples/notebooks/generated/stationarity_detrending_adf_kpss.html
+    # https://www.machinelearningplus.com/time-series/arima-model-time-series-forecasting-python/
+
+    md.append('### Augmented Dickey-Fuller unit root test')
+    md.append(
         'https://www.statsmodels.org/dev/generated/statsmodels.tsa.stattools.adfuller.html')
+    md.append('\n')
 
-    tsa_tools.adfuller(ts_train)
-    tsa_tools.adfuller(detrend_ts_train)
+    md.append(
+        '"The Augmented Dickey-Fuller test can be used to test for a unit root '
+        'in a univariate process in the presence of serial correlation."')
+    md.append(
+        '"The null hypothesis of the Augmented Dickey-Fuller is that there is '
+        'a unit root, with the alternative that there is no unit root."')
+    md.append('\n')
 
+    adf = tsa_tools.adfuller(ts_train)
+    md.append('--------------------')
+    md.append('Time series training segment ADF')
+    md.append('\n')
+    md.append(
+        f'ADF = {adf[0]}\n, p = {adf[1]}\n, number of lags used = {adf[2]}\n, '
+        f'nobs = {adf[3]}\n')
+    md.append(f'critical values = {adf[4]}')
+    md.append('\n')
+    md.append(
+        'Null hypothesis that there is a unit root clearly not rejected; '
+        'trend is present ')
+    md.append('\n')
+
+    adf = tsa_tools.adfuller(detrend_ts_train)
+    md.append('--------------------')
+    md.append('Detrended time series training segment ADF')
+    md.append('\n')
+    md.append(
+        f'ADF = {adf[0]}\n, p = {adf[1]}\n, number of lags used = {adf[2]}\n, '
+        f'nobs = {adf[3]}\n')
+    md.append(f'critical values = {adf[4]}')
+    md.append('\n')
+    md.append(
+        'Null hypothesis that there is a unit root clearly rejected; '
+        'trend is not present.')
+    md.append('\n')
+
+
+    # 
+    ################################################## 
+    # https://otexts.com/fpp3/stationarity.html
+    # https://www.statsmodels.org/dev/examples/notebooks/generated/stationarity_detrending_adf_kpss.html
+    # https://www.machinelearningplus.com/time-series/arima-model-time-series-forecasting-python/
+
+    md.append('### Kwiatkowski-Phillips-Schmidt-Shin (KPSS) test')
+    md.append(
+    'https://www.statsmodels.org/stable/generated/statsmodels.tsa.stattools.kpss.html')
+    md.append('\n')
+
+    md.append(
+        '"Computes the Kwiatkowski-Phillips-Schmidt-Shin (KPSS) test for the '
+        'null hypothesis that x is level or trend stationary."')
+
+    kpss = tsa_tools.kpss(ts_train)
+    md.append('--------------------')
+    md.append('Time series training segment KPSS')
+    md.append('\n')
+    md.append(
+        f'KPSS = {kpss[0]}\n, p = {kpss[1]}\n, lag truncation = {kpss[2]}\n')
+    md.append(f'critical values = {kpss[3]}')
+    md.append('\n')
+    md.append(
+        '<stdin>:1: InterpolationWarning: The test statistic is outside of '
+        'the range of p-values available in the look-up table. The actual '
+        'p-value is smaller than the p-value returned.')
+    md.append('\n')
+    md.append(
+        'Null hypothesis of stationarity clearly rejected; trend is present ')
+    md.append('\n')
+
+
+    kpss = tsa_tools.kpss(detrend_ts_train)
+    md.append('--------------------')
+    md.append('Detrended Time series training segment KPSS')
+    md.append('\n')
+    md.append(
+        f'KPSS = {kpss[0]}\n, p = {kpss[1]}\n, lag truncation = {kpss[2]}\n')
+    md.append(f'critical values = {kpss[3]}')
+    md.append('\n')
+    md.append(
+        '<stdin>:1: InterpolationWarning: The test statistic is outside of '
+        'the range of p-values available in the look-up table. The actual '
+        'p-value is greater than the p-value returned.')
+    md.append('\n')
+    md.append(
+        'Null hypothesis of stationarity not rejected; trend is not present ')
+    md.append('\n')
 
 
 
